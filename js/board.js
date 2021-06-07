547cff69b1296c391a87ef51d83077502721b1db
EunhaKyeong <eh0634a@gmail.com>
2021-06-07 21:59:51 +0900

    글쓰기 페이지 디자인 구현 완료

---
+++ js/board.js
@@ -0,0 +1,5 @@
+var writeBtn = document.getElementById('write_btn');
+
+writeBtn.onclick = function() {
+    window.location.href='file:///C:/netflix-board/html/writing.html';
+}
---
+++ js/write.js
@@ -0,0 +1,5 @@
+var boardBtn = document.getElementById('board_btn');
+
+boardBtn.onclick = function() {
+    window.location.href='file:///C:/netflix-board/html/board.html';
+};
